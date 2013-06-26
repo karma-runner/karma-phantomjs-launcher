be76dce7d94b8cb52233c914af01d535f4ccf42f
nherzing <nherzing@gmail.com>
2013-06-26 11:30:33 -0400

    Add support for injecting runnerOptions to allow setting PhantomJS options (ex. viewPortSize)

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var fs = require('fs');
 
 
-var PhantomJSBrowser = function(baseBrowserDecorator) {
+var PhantomJSBrowser = function(baseBrowserDecorator, options) {
   baseBrowserDecorator(this);
 
   this._start = function(url) {
     // create the js file, that will open karma
     var captureFile = this._tempDir + '/capture.js';
-    var captureCode = '(new WebPage()).open("' + url + '");';
+    var optionsCode = Object.keys(options || {}).map(function (key) {
+      return 'page.' + key + ' = ' + JSON.stringify(options[key]) + ';';
+    });
+    var captureCode = 'var page = new WebPage();' + optionsCode.join() + ' page.open("' + url + '");';
     fs.writeFileSync(captureFile, captureCode);
 
     // and start phantomjs
@@ -26,7 +29,7 @@ PhantomJSBrowser.prototype = {
   ENV_CMD: 'PHANTOMJS_BIN'
 };
 
-PhantomJSBrowser.$inject = ['baseBrowserDecorator'];
+PhantomJSBrowser.$inject = ['baseBrowserDecorator', 'config.runnerOptions'];
 
 
 // PUBLISH DI MODULE
